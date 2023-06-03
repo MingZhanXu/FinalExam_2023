7f453b4f3d4267a131823620cba8bb0c9dcc8661
MingZhanXu <MingZhan.Xu.vm3@gmail.com>
2023-06-03 17:23:40 +0800

    測試鍵盤按鍵A~Z

--- test_t.py
+++ test_t.py
@@ -1,12 +1,12 @@
 from PPM_window import keyboardWindow
 from PySide6.QtGui import  Qt
 import pytest
-class test_keyboard():
-    #測試設定開始時間(正確 回傳值)
+class Test_keyboard():
+    #測試鍵盤按鍵A~Z
     @pytest.mark.parametrize(argnames='keyStr',argvalues = [chr(ord("A") + i)for i in range(23)])
-    def test_keyStr(qtbot, keyStr:str):
+    def test_keyStr(self,qtbot, keyStr:str):
         app = keyboardWindow()
         qtbot.addWidget(app)
         btn = "btn_" + keyStr
         qtbot.mouseClick(getattr(app.ui,btn), Qt.LeftButton)
-        assert app.ui.edit_inquire.text() == keyStr
+        assert app.ui.edit_inquire.text() == keyStr    
