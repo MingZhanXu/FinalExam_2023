e6eeca6c1621cdad3bbd9e07c897b60a3dc1037d
MingZhanXu <MingZhan.Xu.vm3@gmail.com>
2023-06-03 19:28:54 +0800

    增加測試的mark

--- test/Test_PPMWindow.py
+++ test/Test_PPMWindow.py
@@ -5,6 +5,7 @@ import pytest
 @pytest.mark.keyboard
 class Test_keyboard():
     #測試鍵盤按鍵A~Z
+    @pytest.mark.keyboard_AZN
     @pytest.mark.parametrize(argnames='keyStr',argvalues = [chr(ord("A") + i)for i in range(23)])
     def test_key_AZN(self,qtbot, keyStr:str):
         app = keyboardWindow()
@@ -13,6 +14,7 @@ class Test_keyboard():
         qtbot.mouseClick(getattr(app.ui,btn), Qt.LeftButton)
         assert app.ui.edit_inquire.text() == keyStr
     #測試鍵盤輸入文字XXX-NNNN
+    @pytest.mark.keyboard_XXX_NNNN
     @pytest.mark.parametrize(argnames='keyStr, txt',argvalues = [["A10BC35ZX120","ABC-3512"],
                                                                  ["121SAASD3213A","SAA-3213"],
                                                                  ["AS1232131HZ12","ASH-12"],
@@ -29,10 +31,12 @@ class Test_keyboard():
             qtbot.mouseClick(getattr(app.ui,btn), Qt.LeftButton)
         assert app.ui.edit_inquire.text() == txt
     #測試鍵盤del與-填充
+    @pytest.mark.keyboard_del
     @pytest.mark.parametrize(argnames='keyStr, clear, txt',argvalues = [["A10BC35ZX120","000100011000", "AC"],
                                                                         ["121SAASD3213A","1110010011110", "SAS-"],
                                                                         ["121SAASD3213A","0110010011110", "SAS-"],
-                                                                        ["121SAASD3213A","0110010011100", "SAS-3"]])
+                                                                        ["121SAASD3213A","0110010011100", "SAS-3"],
+                                                                        ["121SAASD3213A","0110010011111", "SA"]])
     def test_keyDel(self,qtbot, keyStr:str, clear:str, txt:str):
         app = keyboardWindow()
         qtbot.addWidget(app)
