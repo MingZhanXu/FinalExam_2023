4b10842fb1857c2e98ecec09a00f221856b1762c
MingZhanXu <MingZhan.Xu.vm3@gmail.com>
2023-06-05 20:46:28 +0800

    測試 改為用python執行pytest

--- test/Test_test.py
+++ test/Test_test.py
@@ -4,3 +4,6 @@ import os
 print(os.path.abspath(os.path.dirname(os.getcwd())))
 sys.path.append(rf"{os.path.abspath(os.path.dirname(os.getcwd()))}/FinalExam_2023")
 from lib.PPM.PPM import PPM
+if __name__ == "__main__":
+    pytest.main([f"{os.path.abspath(os.path.dirname(os.getcwd()))}/FinalExam_2023/test/Test_PPM.py"])
+    pytest.main([f"{os.path.abspath(os.path.dirname(os.getcwd()))}/FinalExam_2023/test/Test_PPMWindow.py"])
